24e61237dad40c9b5d162202288c3177494c6bf2
Xyedo <hafidmahdi23@gmail.com>
2023-05-10 12:11:38 +0700

    add aliases

---
+++ pkg/zsh/custom/aliases.zsh
@@ -0,0 +1,11 @@
+#Aliases
+
+## Overwrite existing command
+##  to use actual command without alias
+##  prefix it with backslash (\)
+##  eg. \ls, \rm -rf
+alias rm="trash"
+alias apt="nala"
+alias apt-get="nala"
+alias ls="exa --icons"
+alias cat="ccat"
