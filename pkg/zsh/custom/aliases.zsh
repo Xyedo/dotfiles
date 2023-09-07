e224566821bbb37746df0cb178aeddf7bb3dcf3f
Xyedo <hafidmahdi23@gmail.com>
2023-09-07 15:23:37 +0700

    change alias to a function

--- pkg/zsh/custom/aliases.zsh
+++ pkg/zsh/custom/aliases.zsh
@@ -9,3 +9,11 @@ alias apt="sudo nala"
 alias apt-get="sudo nala"
 alias ls="exa --icons"
 alias cat="ccat"
+
+kubevsms2 () {
+  kubectl port-forward -n vsms-v2-development $(kubectl get pods -n vsms-v2-development | grep 'vsms2-rest-api' | awk '{print $1}') 3001:3000
+}
+
+kubeorder () {
+  kubectl port-forward -n platform-development $(kubectl get pods -n platform-development | grep 'order' | awk '{print $1}') 3000:3000
+}
